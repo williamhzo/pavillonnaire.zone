7e38948ba394cb15ac17d3fd788ab88c199c389a
William Hermozo <william.hermozo@protonmail.com>
2020-06-14 19:44:22 +0200

    initialize map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactMapboxGl from 'react-mapbox-gl';
 import './App.css';
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1Ijoic2FicmlteWxsYXVkIiwiYSI6ImNrYWwyYmxmbzA3cnQyeW15cTY0aTd4cTgifQ.6OY0hboWqf4zuhVXdYtFxw',
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        style="mapbox://styles/sabrimyllaud/ckal2ef6g3fat1ir1jnzuyh6q"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw',
+          center: [2.351027, 48.856669], //   [longitude,latitude]
+          zoom: 12,
+          attributionControl: false,
+        }}
+      ></Map>
     </div>
   );
 }
