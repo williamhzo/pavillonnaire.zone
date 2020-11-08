0fec3292995a487465e9f7e382f96e0b479759d9
William Hermozo <william.hermozo@protonmail.com>
2020-11-08 18:38:06 +0100

    chore: add blend mode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,13 @@ module.exports = {
     },
     extend: {},
   },
-  variants: {},
-  plugins: [],
+  variants: {
+  // all the following default to ['responsive']
+    mixBlendMode: ['responsive'],
+    backgroundBlendMode: ['responsive'],
+    isolation: ['responsive'],
+  },
+  plugins: [
+    require('tailwindcss-blend-mode')(),
+  ],
 }
