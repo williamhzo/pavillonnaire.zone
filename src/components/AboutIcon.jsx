42a10619429627ccc961168b72b18e188d599dff
William Hermozo <william.hermozo@protonmail.com>
2021-05-02 21:32:55 +0200

    style: fix glitch on about icon group hober

--- src/components/AboutIcon.jsx
+++ src/components/AboutIcon.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Instagram({ handleClick }) {
   return (
-    <a
+    <div
       onClick={handleClick}
-      className=" fill-current text-white blend-difference absolute z-10 w-9 h-9 left-6 top-6 border border-white flex justify-center items-center cursor-pointer group"
+      className=" fill-current text-white blend-difference absolute z-20 w-9 h-9 left-6 top-6 border border-white flex justify-center items-center group cursor-pointer"
     >
-      <div className="bg-white w-3 h-3 transform rotate-45 group-hover:rotate-0 transition duration-300 ease-in-out" />
-    </a>
+      <div className="bg-white w-3 h-3 transform rotate-45 group-hover:rotate-0 transition duration-300 ease-in-out"></div>
+    </div>
   );
 }
