0aba71e41959c7cacf5de9419943abf6a6ed935b
William H <william.hermozo@protonmail.com>
2022-10-20 18:11:06 +0200

    feat: toggle DetailsDialog on click

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { useMapBox } from '../hooks/useMapBox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import DetailsModal from './DetailsModal';
 
 type MapProps = {
   hide: boolean;
 };
 
 export default function Map({ hide }: MapProps) {
-  const { mapContainerRef } = useMapBox();
+  const { mapContainerRef, feature } = useMapBox();
 
   return (
-    <div
-      className={`map-container w-full h-full relative ${
-        hide ? 'hidden' : null
-      }`}
-      ref={mapContainerRef}
-    />
+    <>
+      <div
+        className={`map-container w-full h-full relative ${
+          hide ? 'hidden' : ''
+        }`}
+        ref={mapContainerRef}
+      >
+        {feature ? <DetailsModal feature={feature} /> : null}
+      </div>
+    </>
   );
 }
--- src/hooks/useMapBox.tsx
+++ src/hooks/useMapBox.tsx
@@ -1,11 +1,12 @@
 import useMediaQuery from './useMediaQuery';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { MapboxGeoJSONFeature } from 'mapbox-gl';
 import Tooltip from '../components/Tooltip';
 
 const MAPBOX_STYLE = 'mapbox://styles/sabrimyllaud/ckcavaw0y4hx81ipjdzbdw1up';
+// TODO: Should be env vars
 const MAPBOX_API_TOKEN =
   'pk.eyJ1Ijoic2FicmlteWxsYXVkIiwiYSI6ImNrYWwyYmxmbzA3cnQyeW15cTY0aTd4cTgifQ.6OY0hboWqf4zuhVXdYtFxw';
 const INITIAL_LONGITUDE = 1.872;
@@ -16,6 +17,8 @@ const ZOOM_LIMIT = 3;
 const LAYERS = ['musique', 'audiovisuel', 'edition', 'photographie'];
 
 export function useMapBox() {
+  const [feature, setFeature] = useState<MapboxGeoJSONFeature | undefined>();
+
   const isLaptop = useMediaQuery('(min-width: 1024px)');
 
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
@@ -67,6 +70,7 @@ export function useMapBox() {
       }
 
       const feature = features[0];
+      setFeature(feature);
 
       // Create tooltip node
       const tooltipNode = document.createElement('div');
@@ -81,13 +85,34 @@ export function useMapBox() {
 
     // desktop
     map.on('mousemove', renderTooltip);
+    // TODO: necessary?
     // mobile
-    map.on('touchstart', renderTooltip); // FIXME: should be toggled at click (not only touch start)
+    // map.on('touchstart', renderTooltip); // FIXME: should be toggled at click (not only touch start)
+
+    function toggleDetailsDialog(
+      event: mapboxgl.MapMouseEvent & mapboxgl.EventData
+    ) {
+      const dialog = document.getElementById('details-dialog');
+
+      const features = map.queryRenderedFeatures(event.point, {
+        layers: LAYERS,
+      });
+
+      if (!features.length) {
+        dialog?.classList.add('hidden');
+        setFeature(undefined);
+        return;
+      }
+
+      dialog?.classList.remove('hidden');
+    }
+
+    map.on('click', toggleDetailsDialog);
 
     // Clean up on unmount
     return () => map.remove();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLaptop]);
 
-  return { mapContainerRef };
+  return { mapContainerRef, feature };
 }
