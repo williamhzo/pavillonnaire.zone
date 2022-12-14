b24a90fabf44da9f51679fcda98a361ba8c6c4bb
William H <william.hermozo@protonmail.com>
2022-12-14 10:08:20 -0400

    fix vertical scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import Div100vh from 'react-div-100vh';
-
 import SEO from '../components/SEO';
 import Map from '../components/Map';
 import About from '../components/About';
@@ -14,24 +11,22 @@ export default function Homepage() {
   return (
     <>
       <SEO />
-      <main className="w-full relative bg-black">
-        <Div100vh>
-          <AboutIcon handleClick={toggleAbout} />
+      <main className="main-container-height w-full relative bg-black">
+        <AboutIcon handleClick={toggleAbout} />
 
-          <h1 className="flyToTextButton fixed font-serif text-lg text-white blend-difference text-center w-full mt-7 z-10">
-            <a href="http://www.pavillonnaire.zone/">pavillonnaire.zone</a>
-          </h1>
+        <h1 className="flyToTextButton fixed font-serif text-lg text-white blend-difference text-center w-full mt-7 z-10">
+          <a href="http://www.pavillonnaire.zone/">pavillonnaire.zone</a>
+        </h1>
 
-          <Map hide={showAbout} />
+        <Map hide={showAbout} />
 
-          {showAbout && (
-            <div className="h-full w-full flex justify-center items-start p-6 py-20 md:p-20">
-              <About />
-            </div>
-          )}
+        {showAbout && (
+          <div className="h-full w-full flex justify-center items-start p-6 py-20 md:p-20">
+            <About />
+          </div>
+        )}
 
-          <Instagram />
-        </Div100vh>
+        <Instagram />
       </main>
     </>
   );
