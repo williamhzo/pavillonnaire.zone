bb4925774663defede78c3396d03050d0bcf4208
William H <william.hermozo@protonmail.com>
2023-06-04 20:39:48 +0100

    clean up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
-  media: false,
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
     fontFamily: {
       display: ['Gilroy', 'Arial', 'sans-serif'],
       body: ['Alice', 'Arial', 'sans-serif'],
       serif: ['Yoster-Island', 'serif'],
     },
-    extend: {},
-  },
-  variants: {
-    // all the following default to ['responsive']
-    mixBlendMode: ['responsive'],
-    backgroundBlendMode: ['responsive'],
-    isolation: ['responsive'],
-    extend: {},
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
