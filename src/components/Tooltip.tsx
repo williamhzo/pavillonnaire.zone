296b30c13378fd2d1f14f4399ad719e4f8757a82
William H <william.hermozo@protonmail.com>
2023-10-06 15:49:52 +0200

    new types

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { cn } from '@/src/utils';
 import { ComponentProps, FC } from 'react';
-import { cn } from 'utils';
 
 function Text({ children, className }: ComponentProps<'p'>) {
   return <p className={cn('text-center text-sm', className)}>{children}</p>;
@@ -15,12 +15,14 @@ export const Tooltip: FC<TooltipProps> = ({ feature }) => {
   const { title, type, author, director, artist, album, editor, year, place } =
     feature.properties || {};
 
+  const types: string[] = type.split(' ; ').join(', ');
+
   return (
     <div id={title} className="min-w-fit max-w-sm font-body scrollbar-hide">
       <h3 className="mb-2 text-center font-serif text-base leading-6">
         {title}
       </h3>
-      <Text className="italic">{type}</Text>
+      <Text className="italic lowercase">{types}</Text>
       <Text>{author}</Text>
       <Text>{director}</Text>
       <Text>{artist}</Text>
