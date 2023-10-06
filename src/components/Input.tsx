7b07c08aebadcd5f3d9be0d240095337c7fbca11
William H <william.hermozo@protonmail.com>
2023-10-06 17:17:50 +0200

    italicise placeholder

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,7 +10,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full border border-white bg-black px-3 py-2 ring-offset-white placeholder:text-muted focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full border border-white bg-black px-3 py-2 ring-offset-white placeholder:text-muted placeholder:italic focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
