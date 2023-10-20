f54f87add5ae44f5fa51afeeafc113c413d3ff2c
William H <william.hermozo@protonmail.com>
2023-10-20 18:15:40 +0200

    add wip route handler

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,18 +5,17 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  console.log('request', request.json);
+  console.log('request', request.body);
   try {
     const data = await resend.emails.send({
       from: 'Contribution Pav Zone <contribuer@pavillonnaire.zone>',
-      to: ['hi@williamhzo.me'],
+      to: ['contribuer@pavillonnaire.zone'],
       subject: 'Nouvelle contrib!',
-      react: EmailTemplate({ firstName: 'prout' }),
+      react: EmailTemplate(request.body),
       text: 'Hello world',
     });
 
     return NextResponse.json(data);
-    return null;
   } catch (error) {
     return NextResponse.json({ error });
   }
