c27b86476918947640d6138d926efac7d1ac0279
William H <william.hermozo@protonmail.com>
2023-10-27 17:49:42 +0200

    fix undefined type

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatTypeString(type: string) {
+export function formatTypeString(type: string | undefined) {
+  if (!type) return '';
   // TODO: ajouter le "et" sur le dernier si plus d'un.
   return type.split(' ; ').join(', ');
 }
