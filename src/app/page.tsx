79f22d5451dc9649daf1dff9ef7c1be92fac1148
William H <william.hermozo@protonmail.com>
2023-10-27 18:00:45 +0200

    force refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Homepage from '@/components/Homepage';
 import { Instagram } from '@/components/Instagram';
-import Link from 'next/link';
 import { ROOT_PATH } from '@/paths';
 
 export default function Home() {
   return (
     <main className="relative h-[100dvh] w-full overflow-hidden bg-black">
       <h1 className="fixed z-10 mt-7 w-full text-center font-serif text-lg text-white mix-blend-difference">
-        <Link href={ROOT_PATH}>pavillonnaire.zone</Link>
+        <a href={ROOT_PATH}>pavillonnaire.zone</a>
       </h1>
 
       <Homepage />
